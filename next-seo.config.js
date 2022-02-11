37b045ccfbe68eaf94ed8ef1ea2f42937134cd3e
Muhammad Rizqi Tsani <68275535+rizqitsani@users.noreply.github.com>
2022-02-11 19:30:29 +0700

    feat: initial commit

---
+++ next-seo.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next-seo').DefaultSeoProps} */
+const DEFAULT_SEO = {
+  title: undefined,
+  titleTemplate: '%s | Learn Next GraphQL',
+  defaultTitle: 'Learn Next GraphQL',
+  description: 'Learn how to connect Next.js to GraphQL backend',
+  canonical: 'https://learn-next-graphql.vercel.app/',
+  openGraph: {
+    url: 'https://learn-next-graphql.vercel.app/',
+    type: 'website',
+    title: `Learn Next GraphQL`,
+    description: `Learn how to connect Next.js to GraphQL backend`,
+    site_name: 'Learn Next GraphQL',
+  },
+};
+
+export default DEFAULT_SEO;
